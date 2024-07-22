b44ea7ac02e5eb42b4bb523b17bfd6498bf87130
Minoru <danya_naumov_01@mail.ru>
2024-07-22 17:09:56 +0300

    update main

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,8 @@
 import Hero from '../../components/Main/Hero';
 import Start from '../../components/Main/Start';
 
+import './Main.scss';
+
 const Main = () => {
   return (
     <div className="main">
