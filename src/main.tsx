04c8badcfe72b509b8b03d752df03aa60025c980
Minoru <danya_naumov_01@mail.ru>
2024-07-22 14:57:55 +0300

    add router to SPA

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import App from './App/App.tsx';
+import { HashRouter } from 'react-router-dom';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
