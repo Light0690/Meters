d20f4bc0d8291d33b83c3f8e3d5e51f2f4a7c944
Minoru <danya_naumov_01@mail.ru>
2024-07-23 16:36:45 +0300

    update styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
+import '@src/scss/index.scss';
+import '@src/scss/base/index.scss';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <HashRouter>
     <App />
