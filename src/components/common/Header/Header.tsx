3c680cb7040d9ba7f36a9659f477a0c3f0e36b8d
Minoru <danya_naumov_01@mail.ru>
2024-07-24 15:57:22 +0300

    add animate header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
 import cn from 'classnames';
 
 import UXMenu from '@src/components/UX/UXMenu';
@@ -7,16 +9,24 @@ import heroImg from '@assets/Header/logo.png';
 
 import styles from './Header.module.scss';
 
-
 interface props {
   type?: 'transparent'
 }
 
 const Header = ({ type = 'transparent' } : props) => {
   const list = ['Студии', 'Стать инвестором', 'О нас', 'Отзывы', 'Контакты'];
-  
+
+  const header = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({ delay: 1.8 });
+
+    if(header.current) tl.from(header.current, { yPercent: -25, opacity: 0, duration: 2, ease: 'circ.out' })
+  },[])
+
+
   return (
-    <header className={cn(styles.header, styles[type])}>
+    <header ref={header} className={cn(styles.header, styles[type])}>
       <NavLink className={styles['header__container-logo']} to={'/'}>
         <div className={styles.header__logo}>
           <img className={styles['header__img']} src={heroImg} alt="logo" />
