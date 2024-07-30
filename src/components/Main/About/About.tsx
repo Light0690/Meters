8edfdf5d36b908ec1fdfc8f31ee15fb2adc5c6a7
Minoru <danya_naumov_01@mail.ru>
2024-07-30 19:58:53 +0300

    add animate

--- src/components/Main/About/About.tsx
+++ src/components/Main/About/About.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+import splitTypeHelper from '@src/utils/splitTypeHelper';
+
 import aboutIcon from '@assets/Main/about.png';
 
 import styles from './About.module.scss';
 
+
 const About = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  let parentRef = useRef(null);
+  let titleRef = useRef<null | HTMLHeadingElement>(null);
+  let textsRef = useRef<null | HTMLDivElement>(null);
+  let presentationRef = useRef(null);
+  let iconRef = useRef(null);
+  let listRef = useRef<null | HTMLUListElement>(null);
+
   const about = {
     title: 'МЕТРЫ В ЦИФРАХ',
     texts: [
@@ -35,26 +50,46 @@ const About = () => {
       },
     ]
   }
+
+  useEffect(() => {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: parentRef.current,
+        start: `-20% 60%`,
+        end: `60% 60%`,
+        scrub: 1,
+        markers: true
+      },
+    });
+
+    if(titleRef.current) splitTypeHelper(titleRef.current, tl)
+    if(textsRef.current) tl.from(textsRef.current.children, { x: -50, opacity: 0, duration: 1, ease: 'circ.out', stagger: 0.2 }, '<+=0.5')
+    if(presentationRef.current) tl.from(presentationRef.current, { xPercent: -10, opacity: 0, duration: 1, ease: 'circ.out' }, '<+=50%')
+    if(iconRef.current) tl.from(iconRef.current, { yPercent: 30, opacity: 0, duration: 1, ease: 'circ.out' }, '<+=50%')
+    if(listRef.current) tl.from(listRef.current.children, { x: -50, opacity: 0, duration: 1, ease: 'circ.out', stagger: 0.5 }, '0')
+  },[])
   
   return (
-    <div className={styles.about}>
+    <div className={styles.about} ref={parentRef}>
       <div className={styles.about__content}>
         <div className={styles.about__container}>
-          <h2 className={styles.about__title}>{about.title}</h2>
-          <div className={styles.about__texts}>
+          <h2 className={styles.about__title} ref={titleRef}>{about.title}</h2>
+          <div className={styles.about__texts} ref={textsRef}>
             {about.texts.map((text,id) => {
               return <p className={styles.about__text} key={id}>{text}</p>
             })}
           </div>
         </div>
-        <div className={styles.about__presentation}>
+        <div className={styles.about__presentation} ref={presentationRef}>
           <div className={styles['about__img-container']}>
-            <img src={about.img.src} alt={about.img.alt} className={styles.about__img}/>
+            <div className={styles.about__img} ref={iconRef}>
+              <img src={about.img.src} alt={about.img.alt} />
+            </div>
           </div>
           <button className={styles.about__button}>{about.button.text}</button>
         </div>
       </div>
-      <ul className={styles.about__list}>
+      <ul className={styles.about__list} ref={listRef}>
         {about.list.map((li, id) => {
           return (
             <li className={styles.about__li} key={id}>
