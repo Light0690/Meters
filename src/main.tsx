c0a6beaddef39048f90cd2d98ce538adb565e6bf
Minoru <danya_naumov_01@mail.ru>
2024-07-31 16:12:48 +0300

    add register plugin to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import App from './App';
 
 import '@src/scss/base/index.scss';
+
+gsap.registerPlugin(ScrollTrigger);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <HashRouter>
