34fc117d42da8536d607fca59d73a5a376e225eb
Minoru <danya_naumov_01@mail.ru>
2024-08-01 20:17:41 +0300

    separating the animation from the component

---
+++ src/components/Main/Hero/animate.ts
@@ -0,0 +1,33 @@
+import { gsap } from 'gsap';
+
+import { matchMediaCreator } from '@utils/matchMediaCreator';
+import { getDevice } from '@utils/getDevice';
+import useRem from '@utils/useRem';
+
+import { deviceType } from '@src/types/device';
+
+const getX = (device: deviceType) => {
+  if(device === 'desktop') return useRem(-50);
+  if(device === 'laptop')  return useRem(-40);
+  if(device === 'tablet')  return useRem(-30);
+  if(device === 'mobile')  return useRem(-20);
+}
+
+const getDataAnimate = (device: deviceType) => ({ x: getX(device), opacity: 0, duration: 2, ease: 'circ.out', stagger: 0.1 });
+
+interface props {
+  titles:  undefined | HTMLCollection,
+  list:    undefined | HTMLCollection,
+  buttons: undefined | HTMLCollection
+}
+
+const animate = ({ titles, list, buttons }: props) => {
+  const tl = gsap.timeline({ delay: 1.8 });
+  const device = getDevice();
+
+  if(titles)  tl.from(titles, getDataAnimate(device), '0');
+  if(list)    tl.from(list, getDataAnimate(device), device === 'desktop' || device === 'laptop' ? '0' : '<+=0.2');
+  if(buttons) tl.from(buttons, getDataAnimate(device), '0');
+};
+
+export default (props: props) => matchMediaCreator(() => animate(props));
