4f050984798af69cc9d2402520beb37e551b6cc1
Minoru <danya_naumov_01@mail.ru>
2024-08-01 19:49:08 +0300

    delete excess code

--- src/components/Main/Hero/Hero.tsx
+++ src/components/Main/Hero/Hero.tsx
@@ -15,8 +15,6 @@ const Hero = () => {
   const list = useRef<null | HTMLUListElement>(null);
   const buttons = useRef<null | HTMLDivElement>(null);
   const { isScreenMd } = useResize();
-
-  console.log(useResize())
   
   useEffect(() => {
     const tl = gsap.timeline({ delay: 1.8 });
